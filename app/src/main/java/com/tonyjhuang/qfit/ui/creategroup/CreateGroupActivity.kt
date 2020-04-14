e327b2c200287f97203fa73136ffd7e276ccd08d
Tony <tony.huang.jun@gmail.com>
2020-04-14 14:46:02 -0700

    pass group name to create group frag

--- app/src/main/java/com/tonyjhuang/qfit/ui/creategroup/CreateGroupActivity.kt
+++ app/src/main/java/com/tonyjhuang/qfit/ui/creategroup/CreateGroupActivity.kt
@@ -2,6 +2,8 @@ package com.tonyjhuang.qfit.ui.creategroup
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.tonyjhuang.qfit.R
 import kotlinx.android.synthetic.main.activity_create_group.*
 
@@ -11,6 +13,14 @@ class CreateGroupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_group)
+
+        val navHostFragment = nav_host_fragment as NavHostFragment
+
+        val inflater = navHostFragment.navController.navInflater
+        val graph = inflater.inflate(R.navigation.nav_create_group)
+        navHostFragment.navController.setGraph(graph, Bundle().apply {
+            putString(ARG_GROUP_NAME, intent.getStringExtra(ARG_GROUP_NAME))
+        })
     }
 
     companion object {
--- app/src/main/java/com/tonyjhuang/qfit/ui/creategroup/CreateGroupFragment.kt
+++ app/src/main/java/com/tonyjhuang/qfit/ui/creategroup/CreateGroupFragment.kt
@@ -6,21 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.tonyjhuang.qfit.R
+import kotlinx.android.synthetic.main.fragment_create_group.*
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class CreateGroupFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_group, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        group_name.setText(arguments?.getString(CreateGroupActivity.ARG_GROUP_NAME) ?: "")
+
+        save.setOnClickListener {
+
+        }
     }
 }
