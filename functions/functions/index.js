5e285d24dfdd427764151bf23c4fe155477fced1
Tony <tony.huang.jun@gmail.com>
2020-04-21 15:45:35 -0700

    copy user progress to group progress on join group

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -12,21 +12,84 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.deleteGroupCascade = functions.database.ref('/groups/{groupId}')
+exports.deleteGroupCascade = functions
+    .database
+    .ref('/groups/{groupId}')
     .onDelete((snapshot, context) => {
-	const memberIds = snapshot.child('members').val().keys();
-	console.log(memberIds);
-	const groupId = context.params.groupId;
-	console.log(groupId);
-    });
-
-exports.removeGroupMemberCascade = functions.database.ref('/groups/{groupId}/members/{memberId}')
-    .onDelete((snapshot, context) => {
-	const memberId = context.params.memberId;
 	const groupId = context.params.groupId;
 	return snapshot
 	    .ref
 	    .root
-	    .child(`users/${memberId}/groups/${groupId}`)
+	    .child(`group_progress/${groupId}`)
 	    .remove();
     });
+
+exports.removeGroupMemberCascade = functions
+    .database
+    .ref('/groups/{groupId}/members/{memberId}')
+    .onDelete((snapshot, context) => {
+	const memberId = context.params.memberId;
+	const groupId = context.params.groupId;
+	return snapshot
+	      .ref
+	      .root
+	      .child(`users/${memberId}/groups/${groupId}`)
+	      .remove();
+    });
+
+exports.copyUserProgressToGroupProgress = functions
+    .database
+    .ref('/groups/{groupId}/members/{memberId}')
+    .onCreate((snapshot, context) => {
+	const memberId = context.params.memberId;
+	const groupId = context.params.groupId;
+	const dateStr = getDateStr();
+	const root = snapshot.ref.root;
+
+	const getGroupGoalIds = root
+	      .child(`groups/${groupId}/metadata/goals`)
+	      .once('value')
+	      .then((goals) => {
+		  if (goals.exists()) {
+		      return Object.keys(goals.val());
+		  }
+		  return [];
+	      });
+	const getUserProgress = root
+	      .child(`user_progress/${memberId}/${dateStr}`)
+	      .once('value')
+	      .then((userProgress) => {
+		  if (userProgress.exists()) {
+		      return userProgress.val();
+		  }
+		  return {};
+	      });
+
+	function addNewUserGroupProgress(goalId, amount) {
+	    return root
+		.child(`group_progress/${groupId}/${dateStr}/${goalId}/${memberId}/amount`)
+		.set(amount);
+	}
+	
+	return Promise.all([getUserProgress, getGroupGoalIds])
+	    .then((values) => {
+		const [userProgress, groupGoalIds] = values;
+		const updateGroupProgressTasks = [];
+		
+		for (const userGoalId in userProgress) {
+		    if (!groupGoalIds.includes(userGoalId)) {
+			continue;
+		    }
+		    const userProgressAmount = userProgress[groupGoalIds].amount;
+		    updateGroupProgressTasks.push(
+			addNewUserGroupProgress(userGoalId, userProgressAmount));
+		}
+		
+		return Promise.all(updateGroupProgressTasks);
+	    });
+    });
+
+
+function getDateStr() {
+    return new Date().toISOString().slice(0, 10).replace(/-/g,'');
+}
